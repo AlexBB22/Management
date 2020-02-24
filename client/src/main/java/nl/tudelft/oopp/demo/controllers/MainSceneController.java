1ba9d851b496ba732430cea4dc826afdfd0760b2
Niels Tomassen <N.H.C.Tomassen@student.tudelft.nl>
2020-02-24 14:23:09 +0100

    Created a bikeentity and sorting functionality

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -1,7 +1,15 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
+
+import java.io.IOException;
 
 public class MainSceneController {
 
@@ -21,5 +29,13 @@ public class MainSceneController {
         alert.setHeaderText(null);
         alert.setContentText(ServerCommunication.getPun());
         alert.showAndWait();
+    }
+    @FXML
+    public void bikeButtonClicked(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sortByColor.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
