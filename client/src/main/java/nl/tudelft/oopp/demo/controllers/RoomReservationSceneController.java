97febec9036a3a7358d10fd8027319a045505d2e
Hidde Agterberg <H.M.Agterberg@student.tudelft.nl>
2020-02-25 15:26:01 +0100

    Room reservation page layout done

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomReservationSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomReservationSceneController.java
@@ -1,15 +1,31 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RoomReservationSceneController implements Initializable {
+
+    @FXML private DatePicker datePicker;
+    @FXML private ComboBox<String> buildingComboBox;
+    @FXML private ComboBox<String> timeslotComboBox;
+    @FXML private ComboBox<String> roomTypeComboBox;
+
+    @FXML private VBox roomList;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // TODO: populate combo boxes and show available rooms
 
     }
 
+    public void searchButtonHandler(ActionEvent actionEvent) {
 
+    }
 }
