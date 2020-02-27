054f96382bfd81aa9804390a6a256809f30a8025
eli <e.shamayev@student.tudelft.nl>
2020-02-27 15:29:52 +0100

    no message

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Restaurant.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Restaurant.java
@@ -1,4 +1,15 @@
 package nl.tudelft.oopp.demo.entities;
-
+import javax.persistence.*;
+import javax.validation.constraints.*;
+@Entity
+@Table(name = "restaurant")
 public class Restaurant {
+    @Id
+    private int res_id;
+
+    @NotNull
+    @Column(name = "Building")
+    private String buildingName;
+
+    @NotNull
 }
