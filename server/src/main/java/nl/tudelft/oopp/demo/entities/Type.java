2b08a3b50e880f717be23fe07edc38bb2ec9c866
eli <e.shamayev@student.tudelft.nl>
2020-02-27 14:12:43 +0100

    added type entity and removed old imports/db leftovers

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Type.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Type.java
@@ -1,4 +1,79 @@
 package nl.tudelft.oopp.demo.entities;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "type")
 public class Type {
+    @Id
+    private int type_id;
+
+    @NotNull
+    @Column(name = "whiteboard")
+    private boolean whiteBoard;
+
+    @NotNull
+    @Column(name = "tv")
+    private boolean tv;
+
+    @NotNull
+    @Column(name = "power_outlets")
+    private boolean powerOutlets;
+
+    @NotNull
+    @Column(name = "clicker")
+    private boolean clicker;
+
+    @OneToMany(mappedBy = "type", cascade = CascadeType.ALL)
+    private List<Room> listOfRooms;
+
+    public Type(){
+        this.type_id = type_id;
+        this.whiteBoard = whiteBoard;
+        this.powerOutlets = powerOutlets;
+        this.clicker = clicker;
+        listOfRooms = new ArrayList<Room>();
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public boolean isWhiteBoard() {
+        return whiteBoard;
+    }
+
+    public boolean isTv() {
+        return tv;
+    }
+
+    public boolean isPowerOutlets() {
+        return powerOutlets;
+    }
+
+    public boolean isClicker() {
+        return clicker;
+    }
+
+    public void setWhiteBoard(boolean whiteBoard) {
+        this.whiteBoard = whiteBoard;
+    }
+
+    public void setTv(boolean tv) {
+        this.tv = tv;
+    }
+
+    public void setPowerOutlets(boolean powerOutlets) {
+        this.powerOutlets = powerOutlets;
+    }
+
+    public void setClicker(boolean clicker) {
+        this.clicker = clicker;
+    }
+
+    public List<Room> getListOfRooms() {
+        return listOfRooms;
+    }
 }
