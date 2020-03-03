cb9a0eba9b7800cb92f39dcf80f89805d1cd85f1
Niels Tomassen <N.H.C.Tomassen@student.tudelft.nl>
2020-03-03 20:03:31 +0100

    fixed checkstyle errors in the new methods i added

--- server/src/main/java/nl/tudelft/oopp/controllers/RoomReservationController.java
+++ server/src/main/java/nl/tudelft/oopp/controllers/RoomReservationController.java
@@ -48,19 +48,19 @@ public class RoomReservationController {
      * @author Niels Tomassen
      * @param roomId the primary key of the room we want to reserve a room for
      * @param buildingName the primary key of the building we want to reserve a room in
-     * @param Day the Day for which we want to reserve a room is of type sql.Date
-     * @param start_time the start time of the timeslot for which we want to reserve a room
-     * start_time is of type sql.Time
-     * @param end_time the end time of the timeslot for which we want to reserve a room
-     * end_time is of type sql.Time
+     * @param day the day for which we want to reserve a room is of type sql.Date
+     * @param startTime the start time of the timeslot for which we want to reserve a room
+     *                  startTime is of type sql.Time
+     * @param endTime the end time of the timeslot for which we want to reserve a room
+     *                endTime is of type sql.Time
      * @param userId the primary key referring to the user who wants to add a RoomReservation
      * @return RoomReservation the RoomReservation which we are adding to the DB
      */
     @PostMapping(value = "/createNewReservation/{roomId}/{buildingName}/{Day}/{start_time}/{end_time}/{userId}")
     @ResponseBody
     public RoomReservation addRoomReservation(@PathVariable (value = "roomId") int roomId, @PathVariable (value = "buildingName") String buildingName,
-                                              @PathVariable (value = "Day") Date Day, @PathVariable (value = "start_time") Time start_time,
-                                              @PathVariable (value = "end_time") Time end_time, @PathVariable (value = "userId") int userId) {
+                                              @PathVariable (value = "Day") Date day, @PathVariable (value = "start_time") Time startTime,
+                                              @PathVariable (value = "end_time") Time endTime, @PathVariable (value = "userId") int userId) {
 
         //get the Building of a given name
         Optional<Building> b = buildingRepository.findById(buildingName);
@@ -75,7 +75,7 @@ public class RoomReservationController {
         User user = u.get();
 
         //Initialize a timeslot and set up FK entity relationships with Room and Building
-        TimeSlot timeslot = new TimeSlot(start_time, end_time);
+        TimeSlot timeslot = new TimeSlot(startTime, endTime);
         room.addTimeslots(timeslot);
         timeslot.setRoom(room);
         timeslot.setBuilding(building);
@@ -83,7 +83,7 @@ public class RoomReservationController {
         TimeSlot DBtimeslot = timeSlotRepository.save(timeslot);
 
         //Making a new RoomReservation entity and setting up its FK entity relationships with TimeSlot and User
-        RoomReservation roomReservation = new RoomReservation(Day);
+        RoomReservation roomReservation = new RoomReservation(day);
         roomReservation.setUser_fk(user);
         roomReservation.setTimeslot_fk(DBtimeslot);
         user.addRoomReservation(roomReservation);
@@ -98,35 +98,35 @@ public class RoomReservationController {
     /**
      * A method which queries the database to find out which available rooms there are for a certain building at a certain
      * timeslot and date and also checks whether a user can override an existing reservation using the
-     * findAllAvailableRoomsWithOverriding query
+     * findAllAvailableRoomsWithOverriding query.
      *
-     * @param building_name the name of the building in which we want to find all available rooms
-     * @param day the day for which we want to find available rooms
-     * @param start_time the start_time of the timeslot for which we want to find all available rooms day is of type sql.Date
-     * start_time is of type sql.Time
-     * @param end_time the end time of the timeslot for which we want to find all available rooms
-     * end_time is of type sql.Time
-     * @param user_id the primary key of the user who is trying to find all available rooms, this primary key will be used
-     * to determine the role of the user and thus find out if they can override some existing room reservations
-     * @return List<Room> the list of rooms which are available for reserving in a specific building at a specific date and timeslot
-     * for a certain user
+     * @param buildingName the name of the building in which we want to find all available rooms
+     * @param day the day for which we want to find available rooms day is of type sql.Date
+     * @param startTime the start_time of the timeslot for which we want to find all available rooms
+     *                  startTime is of type sql.Time
+     * @param endTime the end time of the timeslot for which we want to find all available rooms
+     *                 endTime is of type sql.Time
+
+     * @param userId the primary key of the user who is trying to find all available rooms, this primary key will be used
+     *               to determine the role of the user and thus find out if they can override some existing room reservations
+     * @return List of rooms the list of rooms which are available for reserving in a specific building at a specific date and timeslot
+     *                  for a certain user
      */
 
     @GetMapping("getAvailableRooms/{buildingName}/{Day}/{start_time}/{end_time}/{user_id}")
     @ResponseBody
-    public List<Room> getAvailableRooms(@PathVariable(value = "buildingName") String building_name,
+    public List<Room> getAvailableRooms(@PathVariable(value = "buildingName") String buildingName,
                                            @PathVariable (value = "Day") Date day,
-                                           @PathVariable (value = "start_time") Time start_time,
-                                           @PathVariable (value = "end_time") Time end_time,
-                                           @PathVariable (value = "user_id") int user_id)
-    {
+                                           @PathVariable (value = "start_time") Time startTime,
+                                           @PathVariable (value = "end_time") Time endTime,
+                                           @PathVariable (value = "user_id") int userId) {
         //Initializes a user from the given user primary key
-        Optional<User> u = userRepository.findById(user_id);
+        Optional<User> u = userRepository.findById(userId);
         User user = u.get();
 
         //Queries the database using the role of the just initialized user
-        List<Integer> objects = roomReservationRepository.findAllAvailableRoomsWithOverriding(building_name, day, start_time,
-                end_time, user.getRole().getRole_id());
+        List<Integer> objects = roomReservationRepository.findAllAvailableRoomsWithOverriding(buildingName, day, startTime,
+                endTime, user.getRole().getRole_id());
 
         List<Room> rooms = new ArrayList<>();
         for (int i = 0; i < objects.size(); i++) {
--- server/src/main/java/nl/tudelft/oopp/repositories/RoomReservationRepository.java
+++ server/src/main/java/nl/tudelft/oopp/repositories/RoomReservationRepository.java
@@ -37,24 +37,24 @@ public interface RoomReservationRepository extends JpaRepository<RoomReservation
      * than the current reservation for a given timeslot.
      *
      * @author Niels Tomassen
-     * @param building_name the name of the building in which we are checking for avaialble rooms
+     * @param buildingName the name of the building in which we are checking for avaialble rooms
      * @param day the day for which we are checking for available rooms is of type sql.Date
-     * @param start_time the start time of a certain timeslot we are looking for rooms to reserve
-     * start_time is of type sql.Time
-     * @param end_time the end time of a certain timeslot we are looking for rooms to reserve
-     * end_time is of type sql.Time
-     * @param role_fk the role of the user who wants to get a list of all availble rooms, used to check
-     * whether this user can override existing roomreservations
-     * @return List<Integer> a list of Integers containing the primary keys of all rooms that are available in
-     * a certain building for a specific timeslot at a specific date
+     * @param startTime the start time of a certain timeslot we are looking for rooms to reserve
+     *                   startTime is of type sql.Time
+     * @param endTime the end time of a certain timeslot we are looking for rooms to reserve
+     *                endTime is of type sql.Time
+     * @param roleFk the role of the user who wants to get a list of all availble rooms, used to check
+     *               whether this user can override existing roomreservations
+     * @return List of Integers containing the primary keys of all rooms that are available in
+     *              a certain building for a specific timeslot at a specific date
      */
-    @Query(value = "SELECT room_id FROM room WHERE room.building_name = ?1 AND (room_id NOT IN " +
-            "(SELECT room_id FROM roomreservation NATURAL JOIN timeslot NATURAL JOIN room" +
-            " WHERE room.building_name = ?1 AND roomreservation.day = ?2 AND (timeslot.start_time = ?3 AND timeslot.end_time = ?4))) OR" +
-            " (room_id IN (SELECT room_id FROM roomreservation NATURAL JOIN timeslot NATURAL JOIN room NATURAL JOIN user WHERE" +
-            " room.building_name = ?1 AND roomreservation.day = ?2 AND (timeslot.start_time = ?3 AND timeslot.end_time = ?4)" +
-            " AND user.role_fk < ?5))", nativeQuery = true)
-    List<Integer> findAllAvailableRoomsWithOverriding(String building_name, Date day, Time start_time, Time end_time, int role_fk);
+    @Query(value = "SELECT room_id FROM room WHERE room.building_name = ?1 AND (room_id NOT IN "
+            + "(SELECT room_id FROM roomreservation NATURAL JOIN timeslot NATURAL JOIN room"
+            + " WHERE room.building_name = ?1 AND roomreservation.day = ?2 AND (timeslot.start_time = ?3 AND timeslot.end_time = ?4))) OR"
+            + " (room_id IN (SELECT room_id FROM roomreservation NATURAL JOIN timeslot NATURAL JOIN room NATURAL JOIN user WHERE"
+            + " room.building_name = ?1 AND roomreservation.day = ?2 AND (timeslot.start_time = ?3 AND timeslot.end_time = ?4)"
+            + " AND user.role_fk < ?5))", nativeQuery = true)
+    List<Integer> findAllAvailableRoomsWithOverriding(String buildingName, Date day, Time startTime, Time endTime, int roleFk);
 
 }
 //write pre-defined methods for SELECTS using distinct
