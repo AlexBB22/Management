5bfed21d7aa69ebe42908982157a0363d296fb5d
eli <e.shamayev@student.tudelft.nl>
2020-03-04 15:40:45 +0100

    minor fixes

--- server/src/test/java/nl/tudelft/oopp/entities/UserTest.java
+++ server/src/test/java/nl/tudelft/oopp/entities/UserTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.oopp.entities;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,4 +31,34 @@ public class UserTest {
         assertEquals("notUsername", user.getUser_name());
         assertEquals(role, user.getRole());
     }
+
+    @Test
+    public void getSetRoomReservations(){
+        User user = new User(2, "email", "username", "password");
+        List<RoomReservation> roomReservList = new ArrayList<RoomReservation>();
+        user.setRoomReservations(roomReservList);
+        assertEquals(roomReservList, user.getRoomReservations());
+    }
+
+    @Test
+    public void addRemoveRoomReservations(){
+        User user = new User(2, "email", "username", "password");
+        List<RoomReservation> roomReservList = new ArrayList<RoomReservation>();
+        Date date = new Date(2020, 3, 10);
+        RoomReservation rs = new RoomReservation(date);
+        user.setRoomReservations(roomReservList);
+        user.addRoomReservation(rs);
+        assertEquals(rs, user.getRoomReservations().get(0));
+        user.removeRoomReservation(rs);
+        assertNotEquals(rs, user.getRoomReservations().get(0));
+    }
+
+    @Test
+    public void toStringTest(){
+        User user = new User(2, "email", "username", "password");
+        String toString =  "user_id: " + user.getUser_id() + " , email: " + user.getEmail()
+                + " , user_name: " + user.getUser_name() + " , user_password: " + user.getUser_password() + " , role_fk: " + user.getRole().getRole_id();
+        assertEquals(toString, user.toString());
+    }
+
 }
