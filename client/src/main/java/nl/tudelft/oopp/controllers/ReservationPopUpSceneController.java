1bc0791cc01439bcfda5ec038225b2dd13b3a630
sajochems <s.a.jochems@student.tudelft.nl>
2020-03-06 15:09:17 +0100

    made the UI for the reservationpopupscene nicer and changed the timeslot selecion for roomreservationscene

--- client/src/main/java/nl/tudelft/oopp/controllers/ReservationPopUpSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/controllers/ReservationPopUpSceneController.java
@@ -1,6 +1,10 @@
 package nl.tudelft.oopp.controllers;
 
+import static nl.tudelft.oopp.MainApp.switchScene;
+
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class ReservationPopUpSceneController {
@@ -8,4 +12,10 @@ public class ReservationPopUpSceneController {
     @FXML private Text room;
     @FXML private Text date;
     @FXML private Text time;
+    @FXML private Text resConfirmed;
+
+    @FXML
+    public void backBtnHandler(MouseEvent mouseEvent) throws IOException {
+        switchScene(mouseEvent, "/roomReservationSceneController.fxml", "Reserve a room");
+    }
 }
