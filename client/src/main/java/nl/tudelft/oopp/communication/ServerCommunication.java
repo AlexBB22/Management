4962cef9357409e43b74905f3688a84a97b05a56
ksartori <k.sartori@student.tudelft.nl>
2020-03-16 16:05:53 +0100

    Added the pop up scene for the bike reservation and the createBikeReservation method in the server Communication class

--- client/src/main/java/nl/tudelft/oopp/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/communication/ServerCommunication.java
@@ -13,6 +13,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Date;
 import java.time.LocalDate;
 
 import java.util.ArrayList;
@@ -193,4 +194,30 @@ public class ServerCommunication {
 
         return response.body();
     }
+
+    public static int createBikeReservation (String buildingName, Date day) throws URISyntaxException {
+        String urlString = String.format("http://localhost:8080/addBikeReservation/%s/%s/%s",
+                buildingName, day,  MainApp.user.getUserId());
+
+        URI url = new URI(urlString);
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(url).header("Content-type", "application/json").POST(HttpRequest.BodyPublishers.ofString("")).build();
+
+        //Sending HTTP Request and getting response
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        if (response.statusCode() != 200) {
+            System.out.println("Error code = " + response.statusCode());
+            return -1;
+        }
+        return 1;
+    }
+
 }
