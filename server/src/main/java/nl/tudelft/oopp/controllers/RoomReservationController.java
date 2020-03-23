70b430f06032d2b41fbefaa2523bca5f23e843df
ksartori <k.sartori@student.tudelft.nl>
2020-03-23 11:49:12 +0100

    Added a delete method that deletes a room reservation.

--- server/src/main/java/nl/tudelft/oopp/controllers/RoomReservationController.java
+++ server/src/main/java/nl/tudelft/oopp/controllers/RoomReservationController.java
@@ -5,11 +5,8 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import nl.tudelft.oopp.entities.Building;
-import nl.tudelft.oopp.entities.Room;
-import nl.tudelft.oopp.entities.RoomReservation;
-import nl.tudelft.oopp.entities.TimeSlot;
-import nl.tudelft.oopp.entities.User;
+
+import nl.tudelft.oopp.entities.*;
 import nl.tudelft.oopp.projections.AvailableRoomProjection;
 import nl.tudelft.oopp.projections.OverridableRoomProjection;
 import nl.tudelft.oopp.projections.UserReservationInfoProjection;
@@ -21,13 +18,7 @@ import nl.tudelft.oopp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-
+import org.springframework.web.bind.annotation.*;
 
 
 @EnableJpaRepositories("nl.tudelft.oopp.repositories")
@@ -280,5 +271,29 @@ public class RoomReservationController {
         return userReservedRooms;
     }
 
+    /**
+     * This method deletes a room reservation.
+     * @param roomReservationID - the id of the reservation that needs to be deleted.
+     */
+    @DeleteMapping ("/deleteRoomReservation/{roomReservationID}")
+    @ResponseBody
+    public void deleteRoomReservation (@PathVariable (value = "roomReservationID") int roomReservationID) {
+
+        try {
+            Optional<RoomReservation> r = roomReservationRepository.findById(roomReservationID);
+            RoomReservation roomReservation = r.get();
+            roomReservation.getUser_fk().removeRoomReservation(roomReservation);
+            roomReservation.setUser_fk(null);
+            roomReservation.getTimeslot_fk().removeRoomReservation(roomReservation);
+            timeSlotRepository.delete(roomReservation.getTimeslot_fk());
+            roomReservation.setTimeslot_fk(null);
+            roomReservationRepository.delete(roomReservation);
+            System.out.println("The reservation has been deleted successfully");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Couldn't delete room reservation");
+        }
+
+
+    }
 
 }
