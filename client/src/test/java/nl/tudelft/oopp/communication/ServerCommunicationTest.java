8b06a64728aa8be50217cb30960d7a84ea686a2c
Kanish Dwivedi <K.Dwivedi-1@student.tudelft.nl>
2020-04-05 18:01:06 +0200

    Added tests for hasher class

--- client/src/test/java/nl/tudelft/oopp/communication/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/communication/ServerCommunicationTest.java
@@ -1,5 +1,130 @@
 package nl.tudelft.oopp.communication;
 
+import org.junit.jupiter.api.Test;
+
 public class ServerCommunicationTest {
 
+    @Test
+    void identifyUser() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getOnlyOverridableRooms() {
+    }
+
+    @Test
+    void getOnlyAvailableRooms() {
+    }
+
+    @Test
+    void getRooms() {
+    }
+
+    @Test
+    void getBuildings() {
+    }
+
+    @Test
+    void getTypes() {
+    }
+
+    @Test
+    void getAllRooms() {
+    }
+
+    @Test
+    void request() {
+    }
+
+    @Test
+    void createBikeReservation() {
+    }
+
+    @Test
+    void createRoomReservation() {
+    }
+
+    @Test
+    void getNumberOfAvailableBikes() {
+    }
+
+    @Test
+    void overrideRoomReservation() {
+    }
+
+    @Test
+    void hasReservation() {
+    }
+
+    @Test
+    void hasBikeReservation() {
+    }
+
+    @Test
+    void getUserReservationInfo() {
+    }
+
+    @Test
+    void bikeReservationList() {
+    }
+
+    @Test
+    void deleteBikeReservation() {
+    }
+
+    @Test
+    void addNewTodo() {
+    }
+
+    @Test
+    void getUserTodoList() {
+    }
+
+    @Test
+    void getRestaurants() {
+    }
+
+    @Test
+    void getAllRestaurants() {
+    }
+
+    @Test
+    void getFoods() {
+    }
+
+    @Test
+    void getFoodReservations() {
+    }
+
+    @Test
+    void createFoodReservation() {
+    }
+
+    @Test
+    void createBuilding() {
+    }
+
+    @Test
+    void createRoom() {
+    }
+
+    @Test
+    void deleteRoomReservation() {
+    }
+
+    @Test
+    void deleteFoodReservation() {
+    }
+
+    @Test
+    void deleteBuilding() {
+    }
+
+    @Test
+    void deleteRoom() {
+    }
 }
---
+++ client/src/test/java/nl/tudelft/oopp/controllers/HasherTest.java
@@ -0,0 +1,24 @@
+package nl.tudelft.oopp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HasherTest {
+
+    @Test
+    void hashPassword1() {
+        String password = "kanish";
+        String res = "c80a11f2ddfff79d56fd0df037f23639a991938328c2f8cc33294d37df4849";
+        assertEquals(res, Hasher.hashPassword(password));
+    }
+
+    @Test
+    void hashPassword2() {
+        String password = "werty7890plkmnbvcxzawe4567ui";
+        int length = Hasher.hashPassword(password).length();
+        assertEquals(62, length);
+    }
+
+
+}
